ae2f3e0d595023348d5323840d90abd7b25dae61
NateXVI <natediven1@gmail.com>
2021-06-29 16:38:21 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const path = require('path');
 const express = require('express');
-const { setgroups } = require('process');
 const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server, {
@@ -14,44 +13,25 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'docs')));
 
-const rooms = {};
+let artists = [];
+let actions = [];
+let redo = [];
 
 io.on('connection', (socket) => {
-	socket.leave(socket.id);
-	console.log(`new connection ${socket.id}`);
-	joinRoom(socket, Number(1).toString());
-	sendAllInstructions(socket);
-
-	socket.on('instruction', (msg) => sendInstruction(socket, msg));
-	socket.on('joinRoom', (msg) => joinRoom(socket, msg));
+	console.log(`New connection ${socket.id}`);
 });
+
+function sync() {
+	io.emit('sync', actions);
+}
+
+setInterval(sync, 5000);
 
 server.listen(port, () => {
 	console.log(`Server is listening on port ${port}`);
 });
 
-function sendInstruction(socket, instruction) {
-	const room = Array.from(socket.rooms)[0];
-	if (!rooms[room]) rooms[room] = [];
-	instruction.index = rooms[room].length + 0.5;
-	// console.log(instruction);
-	rooms[room].push(instruction);
-	socket.to(room).emit('instruction', instruction);
-}
-
-function sendAllInstructions(socket) {
-	const room = Array.from(socket.rooms)[0];
-	socket.emit('set instructions', rooms[room]);
-}
-
-function joinRoom(socket, room) {
-	let rooms = Array.from(socket.rooms);
-	rooms.forEach((v) => {
-		socket.leave(v);
-	});
-	if (room == '') return;
-	console.log(socket.id, 'joined room', room);
-	socket.join(room);
-
-	sendAllInstructions(socket);
+function Room(key, host) {
+	this.key = key;
+	this.host = host;
 }
